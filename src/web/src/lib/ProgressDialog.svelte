ef036920e51532eebf4314ec7f48793bb363f91b
maxid <maximilian.dorninger@sz-ybbs.ac.at>
2025-01-18 21:28:13 +0100

    first step of migrating to svelte

---
+++ src/web/src/lib/ProgressDialog.svelte
@@ -0,0 +1,6 @@
+<dialog class="max active" id="progress-dialog">
+    <main class="responsive absolute center middle">
+        <h5 id="progress-dialog-text">Loading ...</h5>
+        <progress style="margin-top: 5em"></progress>
+    </main>
+</dialog>
