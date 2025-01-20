a649eac2fda8fe78e004cc26306f328bd6a25741
maxid <maximilian.dorninger@sz-ybbs.ac.at>
2025-01-20 19:56:09 +0100

    Automatically refresh user icon in nav bar

--- src/website/src/lib/MainNavBar.svelte
+++ src/website/src/lib/MainNavBar.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
-    import {getCookie} from "../routes/Global.svelte";
+    import {getCookie, User} from "../routes/Global.svelte";
+    import {dev} from "$app/environment";
+    import {onNavigate} from "$app/navigation";
+    import {onMount} from "svelte";
+
+    let baseurl: string = "";
+    if (dev) {
+        baseurl = "http://localhost:8080";
+    }
+
     let copied: boolean = $state(false);
+
     function copyLinktoClipboard() {
         copied = true
         setTimeout(function () {
@@ -8,11 +18,37 @@
         }, 2000)
         navigator.clipboard.writeText(getCookie("user_id"));
     }
+    onMount(async ()=>{
+        auth = await new User().validateLogin()
+        user = new User()
+    })
+    let auth = $state(false);
+    let user: User = $state(new User());
+    onNavigate(async ()=>{
+        let auth2 = new User().validateLogin();
+        let user2 = new User();
+
+        if (auth2 != auth || user2.username != user.username) {
+            auth = await new User().validateLogin();
+            user = new User();
+        }
+    })
+
 </script>
 <nav class="m l left" style="overflow-x: hidden;">
     <a href="/account"
        class="account">
-        <i>account_circle</i>
+        {#await auth}
+            <i>account_circle</i>
+        {:then authenticated}
+            {#if authenticated}
+                <img src="{baseurl}/icons/{user.username}.png"
+                     class="circle"
+                     alt="your profile picture">
+            {:else }
+                <i>account_circle</i>
+            {/if}
+        {/await}
         <div>Your Account</div>
     </a>
     <a href="/">
@@ -53,6 +89,7 @@
     </a>
 
 </nav>
-<div class="snackbar primary absolute center bottom {copied ? 'active' : ''}" id="share_button_snackbar"><i>content_paste</i> Copied link to
+<div class="snackbar primary absolute center bottom {copied ? 'active' : ''}" id="share_button_snackbar"><i>content_paste</i>
+    Copied link to
     clipboard!
 </div>
