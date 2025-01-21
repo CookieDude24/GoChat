79ab8ebb8ae4889785ca1e0f1583e5d537851d3b
maxid <maximilian.dorninger@sz-ybbs.ac.at>
2025-01-21 23:08:12 +0100

    organize directory structure of the repo

--- src/web/src/lib/ProgressDialog.svelte
+++
@@ -1,6 +0,0 @@
-<dialog class="max active" id="progress-dialog">
-    <main class="responsive absolute center middle">
-        <h5 id="progress-dialog-text">Loading ...</h5>
-        <progress style="margin-top: 5em"></progress>
-    </main>
-</dialog>
